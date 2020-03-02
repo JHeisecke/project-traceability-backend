7eec1c9087a1f838a1a31f514df5030acf2ac7e6
Unknown <javierheiseckee@gmail.com>
2020-03-02 19:56:25 -0300

    Inicializacion de proyecto Spring

---
+++ traceability/src/main/java/com/github/pol/una/traceability/TraceabilityApplication.java
@@ -0,0 +1,13 @@
+package com.github.pol.una.traceability;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TraceabilityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TraceabilityApplication.class, args);
+	}
+
+}
---
+++ traceability/src/test/java/com/github/pol/una/traceability/TraceabilityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.pol.una.traceability;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TraceabilityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
