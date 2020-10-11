d83f59184edf9b04def82106fd5d0c874a6f833b
Javier Heisecke <javierheiseckee@gmail.com>
2020-10-11 17:25:53 -0300

    Agrega query para obtencion de usuarios con permisos de team leader

--- traceability/src/main/java/com/github/pol/una/traceability/repository/UsuarioRepository.java
+++ traceability/src/main/java/com/github/pol/una/traceability/repository/UsuarioRepository.java
@@ -1,7 +1,9 @@
 package com.github.pol.una.traceability.repository;
 
+import com.github.pol.una.traceability.dto.UsuarioDTO;
 import com.github.pol.una.traceability.entities.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 import java.util.Optional;
@@ -20,5 +22,14 @@ public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
     Usuario findByUsernameAndPasswordAndEmail(String username, String password, String email);
 
     Optional<Usuario> findById(Long id);
+
+    @Query(value = "SELECT usu.id, usu.nombre_completo " +
+                  "FROM usuario usu " +
+                  "JOIN usuario_rol ur on usu.id = ur.id_usuario " +
+                  "JOIN permiso_rol pr on ur.id_rol = pr.id_rol " +
+                  "WHERE pr.id_recurso = 4 " +
+                  "GROUP BY usu.id, usu.nombre_completo " +
+                  "HAVING COUNT(pr.id_permiso) >= 4", nativeQuery = true)
+    List<Object[]> findTeamLeaders();
 
 }
